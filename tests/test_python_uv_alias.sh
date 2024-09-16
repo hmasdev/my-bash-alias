0c9e36254e6b672bf302746a1a01cd338b1c34a0
hmasdev <hmasuidev1com@gmail.com>
2024-09-17 00:47:30 +0900

    Refactor test_python_uv_alias.sh script

--- tests/test_python_uv_alias.sh
+++ tests/test_python_uv_alias.sh
@@ -8,14 +8,9 @@ source ./myalias.sh
 
 # run
 rm -rf $test_workspace || echo "$test_workspace not found"
-mkdir -p $test_workspace
+uv init $test_workspace
 pushd $test_workspace
 
-python -m venv .venv
-source .venv/bin/activate
-.venv/bin/python -m pip install uv
-
-uv venv
 upip install schedule
 uva ruquests
 uvs
