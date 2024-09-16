2dc5e50366469f1727ec2d9e600f95b51f6279bd
hmasdev <hmasuidev1com@gmail.com>
2024-09-17 00:13:07 +0900

    Add new test scripts and workflows

---
+++ tests/test_basic_alias.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+# const
+test_workspace="./test-workspace"
+test_direc="./test-directory"
+test_file="hoge.txt"
+# setup
+source ./myalias.sh
+
+rm -rf $test_workspace || echo "$test_workspace not found"
+mkdir -p $test_workspace
+pushd $test_workspace
+
+mkd $test_direc
+c $test_direc
+l
+la
+ll
+lla
+psux > /dev/null
+wh bash
+echo $test_direc >> $test_file
+gr $test_direc $test_file
+c ..
+
+popd
+
+# TODO: test k9
---
+++ tests/test_python_uv_alias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+# const
+test_workspace='./test-python_uv_workspace'
+# setup
+source ./myalias.sh
+
+# run
+rm -rf $test_workspace || echo "$test_workspace not found"
+mkdir -p $test_workspace
+pushd $test_workspace
+
+python -m venv .venv
+source .venv/bin/activate
+.venv/bin/python -m pip install uv
+
+uv .venv
+upip install schedule
+uva ruquests
+uvs
+uvr which python
+
+popd
